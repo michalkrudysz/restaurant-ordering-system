d147268936118366cff63331ec28b65067ca9997
Michal <michal@drontrzebownisko.pl>
2024-04-27 12:45:01 +0200

    Created a Context API to manage a modal that displays the cart contents and implemented it in the appropriate components. Additionally, the Context API is prepared for future use to facilitate the checkout process.

---
+++ src/components/Cart.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../utils/formatting";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
+    return totalPrice + item.price * item.quantity;
+  }, 0);
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Twój koszyk</h2>
+      <ul>
+        {cartCtx.items.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.name} - {item.quantity}
+            </li>
+          );
+        })}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button onClick={handleCloseCart} textOnly={true}>
+          Zamknij
+        </Button>
+        <Button onClick={handleCloseCart}>Przejdź do kasy</Button>
+      </p>
+    </Modal>
+  );
+}
