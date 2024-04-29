80a48c1139a799d4c1c5ed4d67fd737b8bc8f25d
Michal <michal@drontrzebownisko.pl>
2024-04-29 11:29:13 +0200

    Added buttons to increase and decrease the quantity of a product in the shopping cart, along with the necessary logic to handle these events.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../utils/formatting";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -21,13 +22,16 @@ export default function Cart() {
     <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Twój koszyk</h2>
       <ul>
-        {cartCtx.items.map((item) => {
-          return (
-            <li key={item.id}>
-              {item.name} - {item.quantity}
-            </li>
-          );
-        })}
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
+        ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
