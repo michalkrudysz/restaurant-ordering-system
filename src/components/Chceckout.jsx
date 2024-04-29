ad6ebabeab93f55614fe7200ca47bfcf07375eec
Michal <michal@drontrzebownisko.pl>
2024-04-29 13:41:16 +0200

    Implemented backend integration to transmit data for completed orders and customer information.

--- src/components/Chceckout.jsx
+++ src/components/Chceckout.jsx
@@ -5,6 +5,7 @@ import UserProgressContext from "../store/UserProgressContext";
 import { currencyFormatter } from "../utils/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
+
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
@@ -16,12 +17,32 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Kasa</h2>
         <p>Całkowita kwota:{currencyFormatter.format(cartTotal)}</p>
-        <Input label="Pełne imię" type="text" id="full-name" />
+        <Input label="Pełne imię" type="text" id="name" />
         <Input label="Adres e-mail" type="email" id="email" />
         <Input label="Ulica" type="text" id="street" />
         <div className="control-row">
