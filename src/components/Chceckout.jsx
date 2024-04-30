f5ac29af7039c7b740e3e9dcaea7bc2e17c9bfa1
Michal <michal@drontrzebownisko.pl>
2024-04-30 15:28:57 +0200

    Added support for sending complex order data, error handling, and displaying information incase of failure, as well as clearing data after placing an order.

--- src/components/Chceckout.jsx
+++ src/components/Chceckout.jsx
@@ -5,10 +5,28 @@ import UserProgressContext from "../store/UserProgressContext";
 import { currencyFormatter } from "../utils/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+
+  const {
+    data,
+    isLoading: isSending,
+    error,
+    sendRequest,
+    clearData,
+  } = useHttp("http://localhost:3000/orders", requestConfig);
+
   const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
     return totalPrice + item.price * item.quantity;
   }, 0);
@@ -17,31 +35,60 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleFinish() {
+    userProgressCtx.hideCheckout();
+    cartCtx.clearCart();
+    clearData();
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    const formData = new FormData(event.target);
+    const customerData = Object.fromEntries(formData.entries());
 
-    const fd = new FormData(event.target);
-    const customerData = Object.fromEntries(fd.entries());
-
-    fetch("http://localhost:3000/orders", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
+    sendRequest(
+      JSON.stringify({
         order: {
           items: cartCtx.items,
           customer: customerData,
         },
-      }),
-    });
+      })
+    );
+  }
+
+  let actions = (
+    <>
+      <Button type="button" textOnly onClick={handleClose}>
+        Zamknij
+      </Button>
+      <Button type="submit">Wyślij zamówienie</Button>
+    </>
+  );
+
+  if (isSending) {
+    actions = <span>Wysyłanie zamówienia...</span>;
+  }
+
+  if (data && !error) {
+    return (
+      <Modal
+        open={userProgressCtx.progress === "checkout"}
+        onClose={handleFinish}
+      >
+        <h2>Zamówienie wysłane</h2>
+        <p>Dziękujemy za złożenie zamówienia!</p>
+        <p className="modal-actions">
+          <Button onClick={handleFinish}>Wyjdź</Button>
+        </p>
+      </Modal>
+    );
   }
 
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
       <form onSubmit={handleSubmit}>
         <h2>Kasa</h2>
-        <p>Całkowita kwota:{currencyFormatter.format(cartTotal)}</p>
+        <p>Całkowita kwota: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Pełne imię" type="text" id="name" />
         <Input label="Adres e-mail" type="email" id="email" />
         <Input label="Ulica" type="text" id="street" />
@@ -49,12 +96,10 @@ export default function Checkout() {
           <Input label="Kod pocztowy" type="text" id="postal-code" />
           <Input label="Miasto" type="text" id="city" />
         </div>
-        <p className="modal-actions">
-          <Button type="button" textOnly onClick={handleClose}>
-            Zamknij
-          </Button>
-          <Button>Wyślij zamówienie</Button>
-        </p>
+        {error && (
+          <Error title="Problem z wysłaniem zamówienia" message={error} />
+        )}
+        <p className="modal-actions">{actions}</p>
       </form>
     </Modal>
   );
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -46,17 +47,15 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
+  if (action.type === "CLEAR_CART") {
+    return { ...state, items: [] };
+  }
+
   return state;
 }
 
 export function CartContextProvider({ children }) {
   const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
-
-  const cartContext = {
-    items: cart.items,
-    addItem,
-    removeItem,
-  };
 
   function addItem(item) {
     dispatchCartAction({ type: "ADD_ITEM", item: item });
@@ -65,7 +64,16 @@ export function CartContextProvider({ children }) {
   function removeItem(id) {
     dispatchCartAction({ type: "REMOVE_ITEM", id });
   }
+  function clearCart() {
+    dispatchCartAction({ type: "CLEAR_CART" });
+  }
 
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+    clearCart,
+  };
   return (
     <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
